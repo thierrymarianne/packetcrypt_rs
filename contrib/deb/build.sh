e5004aa83291c668fb72c572406a11a8f35905f6
Thierry Marianne <thierry@marianne.io>
2021-07-01 17:18:18 +0200

    Remove packetcrypt from version

--- contrib/deb/build.sh
+++ contrib/deb/build.sh
@@ -5,4 +5,4 @@
 # e.g. ./contrib/deb/build.sh
 #
 set -e
-fpm -n packetcrypt-linux -s dir -t deb -v "$(./target/release/packetcrypt --version | sed 's/.* version //' | tr -d '\n')" ./bin
+fpm -n packetcrypt-linux -s dir -t deb -v "$(./target/release/packetcrypt --version | sed -E 's/.* version //' | tr -d '\n')" ./bin
